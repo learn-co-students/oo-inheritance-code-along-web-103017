7242c42e390cfdb4ec9d53932986cfd0e71c3275
crutell331 <tcw331@gmail.com>
2017-11-03 07:51:32 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,17 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+
+  def initialize (size, number)
+    @wheel_size = size
+    @wheel_number = number
+  end
+
+  def go
+    'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+  end
+
+  def fill_up_tank
+    'filling up!'
+  end
+end
