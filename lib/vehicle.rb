45f4e714f6fdcf4bf7ded7f51d9929d8f3fcb930
Silvia Chung <silviachung@Silvias-MacBook.local>
2017-11-06 09:13:28 -0500

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,17 @@
 class Vehicle
+ attr_reader :wheel_size, :wheel_number
+ attr_writer :wheel_size, :wheel_number
 
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+
+  def go
+  "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,15 +1,15 @@
 require "spec_helper"
 
-describe "Vehicle" do 
-  let!(:vehicle) { Vehicle.new("medium", 4) } 
+describe "Vehicle" do
+  let!(:vehicle) { Vehicle.new("medium", 4) }
   describe "#new" do
 
-    it "initializes with two arguments, wheel size and number" do 
+    it "initializes with two arguments, wheel size and number" do
       expect{Vehicle.new("large", 4)}.to_not raise_error
     end
   end
 
-  describe "#wheel_size" do 
+  describe "#wheel_size" do
     it "has a wheel_size" do
       vehicle.wheel_size = "small"
       expect(vehicle.wheel_size).to eq("small")
@@ -17,20 +17,20 @@ describe "Vehicle" do
   end
 
   describe "#wheel_number" do 
-    it "has a wheel_number" do 
-      vehicle.wheel_number = 2      
+    it "has a wheel_number" do
+      vehicle.wheel_number = 2
       expect(vehicle.wheel_number).to eq(2)
     end
   end
 
   describe "#go" do
-    it "returns 'vrrrrrrrooom!'" do 
+    it "returns 'vrrrrrrrooom!'" do
       expect(vehicle.go).to eq("vrrrrrrrooom!")
-    end 
+    end
   end
 
-  describe "#fill_up_tank" do 
-    it "returns 'filling up!'" do 
+  describe "#fill_up_tank" do
+    it "returns 'filling up!'" do
       expect(vehicle.fill_up_tank).to eq("filling up!")
     end
   end
